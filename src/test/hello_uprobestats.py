c887b253e559792c49a373aa857e95d1b220d65b
Matt Gilbride <mattgilbride@google.com>
2024-05-06 20:22:24 +0000

    Fix hello_uprobestats to use resources/tempfiles properly
    - Makes the util work in a stable manner across versions of
      the build system and differences between developer machines.
    - Also tweaks the command line arguments a bit for simplicity.
    
    Bug: 296108553
    Test: m hello_uprobestats && hello_uprobestats
    Change-Id: Ib7f6d89bff126bd1bcd5a10c128a397499b4b5c2

--- src/test/hello_uprobestats.py
+++ src/test/hello_uprobestats.py
@@ -1,108 +1,123 @@
 import argparse
+from importlib import resources
 import os
 import subprocess
 import sys
+import tempfile
 import time
 import config_pb2
 import google.protobuf.text_format as text_format
 
 kwargs = {
-    'text': True,
-    'shell': True,
-    'capture_output': True,
-    'check': True,
+    "text": True,
+    "shell": True,
+    "capture_output": True,
+    "check": True,
 }
 
 
-def get_current_dir():
-  """returns the current dir, relative to the script dir."""
-  current_dir = os.path.dirname(os.path.realpath(__file__))
-  return current_dir
+def adb_root():
+  subprocess.run("adb root", **kwargs)
 
 
-textproto_file = f'{get_current_dir()}/test_slog.textproto'
-pb_file = f'{get_current_dir()}/test_slog.pb'
+def create_and_push_config_proto(name="test_slog"):
+  with resources.open_text("test", f"{name}.textproto") as textproto:
+    message = text_format.Parse(
+        textproto.read(), config_pb2.UprobestatsConfig()
+    )
+    textproto.close()
 
-
-def create_config_proto():
-  textproto = open(textproto_file, 'r')
-  message = text_format.Parse(textproto.read(), config_pb2.UprobestatsConfig())
-  textproto.close()
-
-  pb = open(pb_file, 'wb')
-  pb.write(message.SerializeToString())
-  pb.flush()
-  pb.close()
-
-
-def push_config():
-  print('creating config-slog')
-  config_cmd = (
-      f'adb push {pb_file} /data/misc/uprobestats-configs/config-slog.pb'
-  )
-  subprocess.run(config_cmd, **kwargs)
+    with tempfile.NamedTemporaryFile() as temp:
+      temp.write(message.SerializeToString())
+      temp.flush()
+      print(f"creating {name}")
+      config_cmd = (
+          f"adb push {temp.name} /data/misc/uprobestats-configs/{name}.proto"
+      )
+      subprocess.run(config_cmd, **kwargs)
 
 
 def clear_logcat():
-  print('clearing logcat')
-  subprocess.run('adb logcat -c', **kwargs)
+  print("clearing logcat")
+  subprocess.run("adb logcat -c", **kwargs)
 
 
-def start_uprobestats():
-  print('starting uprobestats')
+def start_uprobestats(name="test_slog"):
+  print("starting uprobestats")
   subprocess.run(
-      'adb shell setprop uprobestats.start_with_config config-slog.pb', **kwargs
+      f"adb shell setprop uprobestats.start_with_config {name}.proto", **kwargs
   )
 
 
-def get_ring_buffer():
-  print('generating log messages and fetching ring buffer size')
-  subprocess.run('adb shell killall com.google.android.apps.photos', shell=True)
-  time.sleep(2)
-  subprocess.run(
-      'adb shell monkey -p com.google.android.apps.photos -c'
-      ' android.intent.category.LAUNCHER 1',
-      **kwargs,
-  )
+def get_ring_buffer_values():
   time.sleep(10)
+  lines = subprocess.run(
+      'adb logcat -d | grep "ringbuf result callback. value:"', **kwargs
+  ).stdout.splitlines()
+  for line in lines:
+    print(line)
+
+
+def get_ring_buffer_size():
+  # print('generating log messages and fetching ring buffer size')
+  # subprocess.run('adb shell killall com.google.android.apps.photos', shell=True)
+  # time.sleep(2)
+  # subprocess.run(
+  #    'adb shell monkey -p com.google.android.apps.photos -c'
+  #    ' android.intent.category.LAUNCHER 1',
+  #    **kwargs,
+  # )
   ring_buffer_size = (
       subprocess.run(
           'adb logcat -d | grep "uprobestats: ring buffer size"', **kwargs
       )
       .stdout.splitlines()[0]
-      .split(':')[4]
+      .split(":")[4]
+      .split(" ")[1]
       .strip()
   )
 
-  print(f'ring buffer size: {ring_buffer_size}')
+  print(f"ring buffer size: {ring_buffer_size}")
   return int(ring_buffer_size, 0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   parser = argparse.ArgumentParser(
-      'Runs uprobestats over adb and checks if things are working'
+      "Drops a uprobestats config over adb and checks start it. Optionally acts"
+      " as a test by checking logcat output"
   )
   parser.add_argument(
-      '-i',
-      '--iterations',
-      type=int,
-      default=0,
-      help='Try n times to get a ring buffer > 0',
+      "-n",
+      "--name",
+      type=str,
+      default="test_slog",
+      help="Name of the config file e.g. test_slog",
+  )
+  parser.add_argument(
+      "-t",
+      "--test",
+      default=False,
+      action="store_true",
+      help=(
+          "Run a test, meaning start uprobestats and then look for particular"
+          " logcat output to exit successfully"
+      ),
   )
   args = parser.parse_args()
-  create_config_proto()
-  push_config()
-  if args.iterations == 0:
-    clear_logcat()
-    start_uprobestats()
-    get_ring_buffer()
+
+  adb_root()
+  create_and_push_config_proto(args.name)
+
+  if not args.test:
+    start_uprobestats(args.name)
     sys.exit(0)
 
-  for _ in range(args.iterations):
-    clear_logcat()
-    start_uprobestats()
-    ring_buf = get_ring_buffer()
-    if ring_buf > 0:
-      sys.exit(0)
-
-  raise SystemExit('Never found a ring buffer greater than zero')
+  clear_logcat()
+  start_uprobestats(args.name)
+  time.sleep(60)
+  ring_buf = get_ring_buffer_size()
+  get_ring_buffer_values()
+  if ring_buf > 0:
+    sys.exit(0)
+  else:
+    raise SystemExit("Did not find a ring buffer greater than zero")
