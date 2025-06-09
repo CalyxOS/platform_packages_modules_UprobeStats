d1970d9cec88bf3eca95497899633aa430ee9186
Yu-Ting Tseng <yutingtseng@google.com>
2025-06-09 13:48:04 -0700

    Allow stateful BPF map handlers
    Currently, the BPF map handlers are stateless. This commit changes them
    so that they can hold state, and this state is preserved across multiple
    calls to `on_item`. This is important for implementing various rate
    limiting mechanisms, where handlers need to be able to "remember" the
    previous calls.
    
    This is achieved by:
    *  Changing the trait `OnItem` to `Handler`, and passing mutable reference to `on_item` method
    * Adding a default implementation to handlers, which create a default mutable object at the begin of the BPF map polling loops.
    
    Bug: 400457896
    Test: m com.google.android.uprobestats && adb install out/soong/.intermediates/packages/modules/UprobeStats/apex/com.android.uprobestats/android_common_com.google.android.uprobestats_com.google.android.uprobestats/com.google.android.uprobestats.apex && adb reboot && atest uprobestats-test
    Flag: EXEMPT refactor
    Change-Id: Iab9dbc8cbfc3ee11a4577df80cf30f1e70c5e703

--- rust/src/bpf_map/disruptive_app.rs
+++ rust/src/bpf_map/disruptive_app.rs
@@ -1,4 +1,4 @@
-use super::{bytes_as_str, OnItem};
+use super::{bytes_as_str, Handler};
 use crate::config_resolver::ResolvedTask;
 use anyhow::Result;
 use log::debug;
@@ -10,16 +10,20 @@ use uprobestats_bpf_bindgen::{BindServiceLocked, ComponentEnabledSetting};
 
 const COMPONENT_ENABLED_STATE_DISABLED: i32 = 2; // PackageManager#COMPONENT_ENABLED_STATE_DISABLED (all values greater than or equal to are disabled states)
 
+#[derive(Default)]
+pub struct ComponentEnabledSettingHandler {}
+
 // SAFETY: `ComponentEnabledSetting` is a struct defined in the given `MAP_PATH`, and is guaranteed to match the
 // layout of the corresponding C struct.
-unsafe impl OnItem for ComponentEnabledSetting {
+unsafe impl Handler for ComponentEnabledSettingHandler {
     const MAP_PATH: &'static str =
         "/sys/fs/bpf/uprobestats/map_DisruptiveApp_ComponentEnabledSetting_output_buf";
-    fn on_item(&self, _task: &ResolvedTask) -> Result<()> {
-        let package_name = bytes_as_str(&self.package_name)?;
-        let class_name = bytes_as_str(&self.class_name)?;
-        let new_state = self.new_state;
-        let calling_package_name = bytes_as_str(&self.calling_package_name)?;
+    type T = ComponentEnabledSetting;
+    fn on_item(&mut self, _task: &ResolvedTask, data: &ComponentEnabledSetting) -> Result<()> {
+        let package_name = bytes_as_str(&data.package_name)?;
+        let class_name = bytes_as_str(&data.class_name)?;
+        let new_state = data.new_state;
+        let calling_package_name = bytes_as_str(&data.calling_package_name)?;
         debug!("ComponentEnabledSetting: package_name={:?}, class_name={:?}, new_state={:?}, calling_package_name={:?}", package_name, class_name, new_state, calling_package_name);
         if new_state < COMPONENT_ENABLED_STATE_DISABLED {
             // < PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
@@ -37,19 +41,23 @@ unsafe impl OnItem for ComponentEnabledSetting {
 
 const BIND_ALLOW_BACKGROUND_ACTIVITY_STARTS: c_long = 0x00100000; // Context.BIND_ALLOW_BACKGROUND_ACTIVITY_STARTS
 
+#[derive(Default)]
+pub struct BindServiceLockedHandler {}
+
 // SAFETY: `BindServiceLocked` is a struct defined in the given `MAP_PATH`, and is guaranteed to match the
 // layout of the corresponding C struct.
-unsafe impl OnItem for BindServiceLocked {
+unsafe impl Handler for BindServiceLockedHandler {
     const MAP_PATH: &'static str =
         "/sys/fs/bpf/uprobestats/map_DisruptiveApp_BindServiceLocked_output_buf";
-    fn on_item(&self, _task: &ResolvedTask) -> Result<()> {
-        let intent_package = bytes_as_str(&self.intent_package)?;
-        let intent_action = bytes_as_str(&self.intent_action)?;
-        let intent_component_name_package = bytes_as_str(&self.intent_component_name_package)?;
-        let intent_component_name_class = bytes_as_str(&self.intent_component_name_class)?;
-        let flags = self.bind_flags;
-        let calling_package = bytes_as_str(&self.calling_package)?;
-        let has_bal_flag = (self.bind_flags & BIND_ALLOW_BACKGROUND_ACTIVITY_STARTS) != 0;
+    type T = BindServiceLocked;
+    fn on_item(&mut self, _task: &ResolvedTask, data: &BindServiceLocked) -> Result<()> {
+        let intent_package = bytes_as_str(&data.intent_package)?;
+        let intent_action = bytes_as_str(&data.intent_action)?;
+        let intent_component_name_package = bytes_as_str(&data.intent_component_name_package)?;
+        let intent_component_name_class = bytes_as_str(&data.intent_component_name_class)?;
+        let flags = data.bind_flags;
+        let calling_package = bytes_as_str(&data.calling_package)?;
+        let has_bal_flag = (data.bind_flags & BIND_ALLOW_BACKGROUND_ACTIVITY_STARTS) != 0;
         debug!(
             "BindServiceLocked: intent_package={:?}, intent_action={:?}, intent_component_name_package={:?}, intent_component_name_class={:?} flags={:?}, calling_package={:?}, has_bal_flag={}",
             intent_package, intent_action, intent_component_name_package, intent_component_name_class, flags, calling_package, has_bal_flag
